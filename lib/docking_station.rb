3f6640e9fb565cc894a8340111462c2f6fec035a
sarpong <abasimisarpong@rocketmail.com>
2019-04-02 16:31:48 +0100

    working on relese_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,7 @@
+require_relative './bike'
+
 class DockingStation
     def relese_bike
+      Bike.new
     end 
 end
--- spec/Docking_Station_spec.rb
+++ spec/Docking_Station_spec.rb
@@ -1,7 +1,6 @@
-require 'docking_station'
 
 RSpec.describe DockingStation do
   describe '#relese_bike' do
-    it { is_expected.to respond_to(:relese_bike) }
+
   end
 end
