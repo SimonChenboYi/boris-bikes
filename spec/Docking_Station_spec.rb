5f733c4403928b45ec666499dce58de3e2993104
Xain Storey <xainstorey@MacBook-Pro.local>
2019-04-03 12:30:29 +0100

    Added instance variables for DockingStation to access

--- spec/Docking_Station_spec.rb
+++ spec/Docking_Station_spec.rb
@@ -2,20 +2,33 @@ require 'docking_station'
 require 'bike'
 
 RSpec.describe DockingStation do
+  RSpec.configure do |c|
+    c.example_status_persistence_file_path = "examples.txt"
+  end
   describe '#release_bike' do
 
     it "should release bike" do
       expect(subject.release_bike).to be_a(Bike)
     end
+  end
 
+  describe '#working?' do
     it "should check if the bike is working" do
       expect(subject.release_bike.working?).to be_truthy
     end
-
-    it "should receive and dock bike" do
-      expect(subject).to respond_to(:dock).with(1).argument
-    end
-
-
   end
+
+  describe '#dock' do
+    bike = Bike.new
+    it "should receive and dock bike" do
+      expect(subject.dock(bike)).to eq bike
+    end
+  end
+
+  describe '#bike' do
+    it "should tell user if there's a docked bike" do
+      expect(subject).to respond_to(:bike)
+    end
+  end
+
 end
