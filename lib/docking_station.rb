178fcf0481906c63b22a7c1c9ec5dc480d2091fe
Xain Storey <xainstorey@MacBook-Pro.local>
2019-04-04 15:57:56 +0100

    Added private methods for testing capacity of docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,13 +8,23 @@ class DockingStation
   end
 
     def release_bike
-      fail "No bikes available" if @bike.empty?
+      fail "No bikes available" if empty?
       @bike.pop
     end
 
 
     def dock(bike)
-      fail "Docking station is full" if @bike.count >= 20
+      fail "Docking station is full" if full?
       @bike << bike
+    end
+
+    private
+
+    def full?
+      @bike.count >= 20
+    end
+
+    def empty?
+      @bike.empty?
     end
 end
